44454964d4d3cb700ac103b2fcd5846fc869f3b7
grigoryan.m <waldm4rk@gmail.com>
2023-08-26 14:29:59 +0400

    name and three-line contacts now works

---
+++ js/cv.js
@@ -0,0 +1,20 @@
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const data = [findGetParameter("name"), findGetParameter("con1"), findGetParameter("con2"), findGetParameter("con3")];
+  document.getElementById("name").innerText = data[0];
+  document.getElementById("phone").innerText = data[1];
+  document.getElementById("email").innerText = data[2];
+  document.getElementById("portfolio").innerText = data[3];
+});
---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button_element = document.getElementById('done');
+
+    button_element.addEventListener('click', () =>{
+
+        const data = [document.getElementById('name').value, document.getElementById('phone').value, document.getElementById('email').value, document.getElementById('portfolio').value]
+
+        let url="file:///home/mishpa/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/cv-generator/src/cv.html?name=" + data[0] + "&con1=" + data[1] + "&con2=" + data[2] + "&con3=" + data[3];
+        const link = document.getElementById('link');
+        link.href = url;
+    });
+
+});
