8e3507725ee1d1247766af2f4a7b025a145cb1e9
grigoryan.m <waldm4rk@gmail.com>
2023-08-27 15:05:15 +0400

    Finished project

--- js/cv.js
+++ js/cv.js
@@ -1,3 +1,4 @@
+// Function to get parameters from the url
 function findGetParameter(parameterName) {
     var result = null,
         tmp = [];
@@ -10,16 +11,22 @@ function findGetParameter(parameterName) {
         });
     return result;
 }
-
+// Waiting until DOM is loaded
 document.addEventListener('DOMContentLoaded', function () {
+
+  // Getting all our get parameters;
   const data = [findGetParameter("name"), findGetParameter("con1"), findGetParameter("con2"), findGetParameter("con3"), findGetParameter("sum"), findGetParameter("skill1"), findGetParameter("skill2"),findGetParameter("skill3"),findGetParameter("skill4"),findGetParameter("skill5"),findGetParameter("skill6")]
+  // Changing our elements values to user data
   document.getElementById("name").innerText = data[0];
   document.getElementById("phone").innerText = data[1];
   document.getElementById("email").innerText = data[2];
   document.getElementById("portfolio").innerText = data[3];
   document.getElementById("summary").innerText = data[4];
-
+  document.getElementById("career").innerHTML = findGetParameter("exp");
+  document.getElementById("lang").innerHTML = findGetParameter("lang")
   for(let i = 5; i < data.length; i++){
     document.getElementById("skills_list").innerHTML += "<li>" + data[i] + "</li>";
   }
+  var element = document.getElementsByClassName('wrapper')[0];
+  html2pdf(element);
 });
--- js/main.js
+++ js/main.js
@@ -3,15 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     button_element.addEventListener('click', () =>{
 
+        // First few sections data (GET parameters)
         const data = [document.getElementById('name').value, document.getElementById('phone').value, document.getElementById('email').value, document.getElementById('portfolio').value, document.getElementById('summary').value];
+        // Generating link with first sections data
+        let url="file:///home/mishpa/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/cv-generator/src/cv.html?name=" + data[0] + "&con1=" + data[1] + "&con2=" + data[2] + "&con3=" + data[3] + "&sum=" + data[4];    
+        
+        // Getting all the data from skills inputs
         let skills = document.getElementsByClassName('skill');
-
-        let url="file:///home/mishpa/%D0%94%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/cv-generator/src/cv.html?name=" + data[0] + "&con1=" + data[1] + "&con2=" + data[2] + "&con3=" + data[3] + "&sum=" + data[4];
         
         for(let i = 0; i < skills.length; i++){
           url += "&skill" + (i+1).toString() + "=" + skills[i].value;  
         }
-
+        // Adding career and expierence, languages to url
+        url += "&exp=" + document.getElementById("career").value + "&lang=" + document.getElementById("languages").value;
+        // Changing href of <a> to our generated link
         const link = document.getElementById('link');
         link.href = url;
     });
